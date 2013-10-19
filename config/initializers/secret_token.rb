f3c0d2712408e35a6ee2e662968b00aab217d70e
Miranda <miranda.hadfield@gmail.com>
2013-10-19 19:09:38 +0100

    Ch_one_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChOneApp::Application.config.secret_key_base = '6e522cbcf10545dba29e562ba70b126934aedb6af389063570d98c32257c2992ae1485d42a0269e11f7585763778143c809e6dc0147b7e38c421674e88550371'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChOneApp::Application.config.session_store :cookie_store, key: '_ch_one_app_session'
